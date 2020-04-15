49826f849262d29cebc588aae0e54e24fe941eb1
Muhammad Afif <hammadaf9408@gmail.com>
2020-04-15 17:06:22 +0700

    Add route for my playground

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { LoadingState } from './context/LoadingState'
 import { Playground } from './playground/Playground'
 // import { Card } from './components/ui/Card'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 export const App = () => {
   return (
     <React.Fragment>
      <LoadingState>
-       <Playground />
+       <Router>
+         <Switch>
+           <Route exact path="/playground" component={Playground} />
+         </Switch>
+       </Router>
      </LoadingState>
      {/* <Card /> */}
     </React.Fragment>
